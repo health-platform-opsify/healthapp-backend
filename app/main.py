6c40151f76901f3c1f8eacbacd0c3af56320392f
Rijo Thomas <rijo.thomas@ust.com>
2025-10-27 17:26:23 +0000

    chore: trigger PR CI

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from app.api.v1.routes.organization import router as org_router
-from app.api.v1.routes.patients import router as patients_router
 
 app = FastAPI(title="HealthApp Backend",
               version="0.1.0",
@@ -9,9 +7,8 @@ app = FastAPI(title="HealthApp Backend",
               )
 
 
-# Include versioned routes
-app.include_router(org_router, prefix="/api/v1", tags=["organizations"])
-app.include_router(patients_router, prefix="/api/v1", tags=["patients"])
+
+
 
 @app.get("/healthz")
 def healthz():
