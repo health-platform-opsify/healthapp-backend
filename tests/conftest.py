81560570afa111e0cb92415d3c0097049dab045f
Rijo Thomas <rijo.thomas@ust.com>
2025-10-27 17:34:49 +0000

    chore: trigger PR CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,6 @@ import asyncio
 from typing import Generator
 import pytest
 
-# Set the event loop fixture scope
-pytest.ini_options = {
-    "asyncio_mode": "auto",
-}
-
 @pytest.fixture(scope="session")
 def event_loop() -> Generator[asyncio.AbstractEventLoop, None, None]:
     """Create an instance of the default event loop for each test case."""
